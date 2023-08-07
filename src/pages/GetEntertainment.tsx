a8abad52b5ebae9d363dc3ec4cb8491109a98952
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-08-07 14:43:27 +0700

    feat: add pages by category

--- src/pages/GetEntertainment.tsx
+++ src/pages/GetEntertainment.tsx
@@ -1,23 +1,33 @@
-import Layout from "../components/Layout";
 import { FC, useState, useEffect } from "react";
-import { Card } from "../components/Card";
-import { HorizontalRuleForPages } from "../components/HorizontalRule";
+import axios from "axios";
 import { NewsType } from "../utils/types";
-import { fetchNews } from "../utils/api";
+import Layout from "../components/Layout";
+import { HorizontalRuleForPages } from "../components/HorizontalRule";
+import { Card } from "../components/Card";
 
-const GetLatest: FC = () => {
+const GetEntertainmnet: FC = () => {
   const [newsDatas, setNewsDatas] = useState<NewsType[]>([]);
-
   useEffect(() => {
-    fetchNews().then((articles) => {
-      setNewsDatas(articles);
-    });
+    axios
+      .get(
+        `${
+          import.meta.env.VITE_BASE_URL
+        }top-headlines?country=us&category=entertainment&apiKey=${
+          import.meta.env.VITE_API_KEY
+        }`
+      )
+      .then((res) => {
+        const { articles } = res.data;
+        setNewsDatas(articles);
+      })
+      .catch((err) => {
+        alert(err.toString());
+      });
   }, []);
-
   return (
     <Layout>
       <div className="px-5 bg-slate-100 my-20">
-        <p className="text-2xl font-semibold py-14">All News</p>
+        <p className="text-2xl font-semibold py-14">Entertainment</p>
         <HorizontalRuleForPages />
         <div className="grid grid-cols-5 gap-16 mt-9">
           {newsDatas.map((newsData, index) => (
@@ -38,4 +48,4 @@ const GetLatest: FC = () => {
   );
 };
 
-export default GetLatest;
+export default GetEntertainmnet;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,23 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Homepage from "../pages/App";
 import GetLatest from "../pages/GetLatest";
+import GetBusiness from "../pages/GetBusiness";
+import GetEntertainmnet from "../pages/GetEntertainment";
+import GetHealth from "../pages/GeatHealth";
+import GetScience from "../pages/GetScience";
+import GetSports from "../pages/GetSports";
+import GetTechnology from "../pages/GetTechnology";
 
 const index: FC = () => {
   const router = createBrowserRouter([
     { path: "/", element: <Homepage /> },
     { path: "/latest", element: <GetLatest /> },
+    { path: "/business", element: <GetBusiness /> },
+    { path: "/entertainment", element: <GetEntertainmnet /> },
+    { path: "/health", element: <GetHealth /> },
+    { path: "/science", element: <GetScience /> },
+    { path: "/sports", element: <GetSports /> },
+    { path: "/technology", element: <GetTechnology /> },
   ]);
   return <RouterProvider router={router} />;
 };
